29a19519dadc5ace16ea9442c3c5a2960b1a092c
Csaba Vadász <csabavadasz79@gmail.com>
2024-02-17 10:55:12 +0100

    update code using ResponseEntity

--- src/main/java/hu/webler/webleruserregistrationandlogin/controller/UserController.java
+++ src/main/java/hu/webler/webleruserregistrationandlogin/controller/UserController.java
@@ -3,6 +3,7 @@ package hu.webler.webleruserregistrationandlogin.controller;
 import hu.webler.webleruserregistrationandlogin.model.UserModel;
 import hu.webler.webleruserregistrationandlogin.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/users")
-    public List<UserModel> renderAllUsers() {
-        return userService.findAllUsers();
+    public ResponseEntity<List<UserModel>> renderAllUsers() {
+        return ResponseEntity.status(200).body(userService.findAllUsers());
     }
 }
