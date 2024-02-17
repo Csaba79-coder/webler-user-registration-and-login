46c3e3a60a1941c4338e7bac6494c10dc9353935
Csaba Vadász <csabavadasz79@gmail.com>
2024-02-17 10:23:04 +0100

    create UserModel for response

---
+++ src/main/java/hu/webler/webleruserregistrationandlogin/model/UserModel.java
@@ -0,0 +1,17 @@
+package hu.webler.webleruserregistrationandlogin.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserModel {
+
+    private Long id;
+    private String email;
+    private String username;
+}
