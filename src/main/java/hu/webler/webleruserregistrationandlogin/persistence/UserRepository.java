12034866ee9e657116d741b6e735ff987cb7f9fb
Csaba Vadász <csabavadasz79@gmail.com>
2024-02-17 10:08:03 +0100

    create User repository

---
+++ src/main/java/hu/webler/webleruserregistrationandlogin/persistence/UserRepository.java
@@ -0,0 +1,9 @@
+package hu.webler.webleruserregistrationandlogin.persistence;
+
+import hu.webler.webleruserregistrationandlogin.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository  extends JpaRepository<User, Long> {
+}
