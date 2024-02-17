3b87a3aaeb47032c4bdc8858fc3296dd9b4ad789
Csaba Vadász <csabavadasz79@gmail.com>
2024-02-17 10:40:23 +0100

    create a function in UserService to find all entity and return the model

--- src/main/java/hu/webler/webleruserregistrationandlogin/service/UserService.java
+++ src/main/java/hu/webler/webleruserregistrationandlogin/service/UserService.java
@@ -1,8 +1,13 @@
 package hu.webler.webleruserregistrationandlogin.service;
 
+import hu.webler.webleruserregistrationandlogin.model.UserModel;
 import hu.webler.webleruserregistrationandlogin.persistence.UserRepository;
+import hu.webler.webleruserregistrationandlogin.util.Mapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +15,10 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    public List<UserModel> findAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(Mapper::mapUserEntityToUserModel)
+                .collect(Collectors.toList());
+    }
 }
