68eba9f02ed1804198c33798d72b512477ed6ee3
Csaba Vadász <csabavadasz79@gmail.com>
2024-02-17 10:46:15 +0100

    create render all users in Controller (rest api)

--- src/main/java/hu/webler/webleruserregistrationandlogin/controller/UserController.java
+++ src/main/java/hu/webler/webleruserregistrationandlogin/controller/UserController.java
@@ -1,12 +1,23 @@
 package hu.webler.webleruserregistrationandlogin.controller;
 
+import hu.webler.webleruserregistrationandlogin.model.UserModel;
 import hu.webler.webleruserregistrationandlogin.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
+
+    @GetMapping("/users")
+    public List<UserModel> renderAllUsers() {
+        return userService.findAllUsers();
+    }
 }
